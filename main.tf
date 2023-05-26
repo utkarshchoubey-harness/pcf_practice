881086c81f34f09c95ed9f9b9deff3fb220330ae
sourabh-awashti <sourabh.awashti@harness.io>
2023-05-26 18:00:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
 output "test-module" {
   value = "test-modue"
 }
+
+resource "null_resource" "cluster" {
+  # Changes to any instance of the cluster requires re-provisioning
+  triggers = {
+    cluster_instance_ids = "cluster_instance_ids"
+  }
+}
